fb5949f4bdcc52a771d583c5c68c4a61b5a4436b
kazu-321 <kzs321kzs@gmail.com>
2025-05-03 17:49:06 +0900

    パラメーター追加

--- src/visualization/nhk2025b_visualize_swerve/src/visualize_swerve.cpp
+++ src/visualization/nhk2025b_visualize_swerve/src/visualize_swerve.cpp
@@ -5,9 +5,19 @@ namespace visualize_swerve {
         : Node("visualize_swerve",options) {
         marker_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("/visualization/swerve", 10);
         swerve_subscriber_ = this->create_subscription<nhk2025b_msgs::msg::Swerve>("/swerve", 10, std::bind(&visualize_swerve::swerve_callback, this, std::placeholders::_1));
+        wheel_position = this->declare_parameter<float>("wheel_position", 0.622);
+        wheel_radius = this->declare_parameter<float>("wheel_radius", 0.062);
     }
 
     void visualize_swerve::swerve_callback(const nhk2025b_msgs::msg::Swerve::SharedPtr msg) {
+        wheel_position = this->get_parameter("wheel_position").as_double();
+        wheel_radius = this->get_parameter("wheel_radius").as_double();
+        float wheel_positions[4][2] = {
+            {+ wheel_position, + wheel_position},
+            {- wheel_position, + wheel_position},
+            {- wheel_position, - wheel_position},
+            {+ wheel_position, - wheel_position}
+        };
         visualization_msgs::msg::MarkerArray marker_array_;
         for(int i = 0; i < 4; i++) {
             visualization_msgs::msg::Marker marker;
