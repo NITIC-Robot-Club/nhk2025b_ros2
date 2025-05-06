16c15288afdc7f07c9e62d851cbb374d63ccfc8c
kazu-321 <kzs321kzs@gmail.com>
2025-05-06 10:50:25 +0900

    add header

--- src/simulation/nhk2025b_simulation/include/nhk2025b_simulation/simulation.hpp
+++ src/simulation/nhk2025b_simulation/include/nhk2025b_simulation/simulation.hpp
@@ -1,6 +1,7 @@
 #ifndef __simulation_hpp__
 #define __simulation_hpp__
 
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <nhk2025b_msgs/msg/robot_status.hpp>
 #include <nhk2025b_msgs/msg/swerve.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -14,10 +15,18 @@ class simulation : public rclcpp::Node {
     void timer_callback ();
     void swerve_callback (const nhk2025b_msgs::msg::Swerve::SharedPtr msg);
 
+    float wheel_radius;
+    float wheel_position;
+    float x_sum_, y_sum_, z_sum_;
+    int   count_;
+    bool  sig_;
+    float x_, y_, z_;
+
     rclcpp::TimerBase::SharedPtr timer_;
 
     rclcpp::Publisher<nhk2025b_msgs::msg::Swerve>::SharedPtr      swerve_publisher_;
     rclcpp::Publisher<nhk2025b_msgs::msg::RobotStatus>::SharedPtr robot_status_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
     rclcpp::Subscription<nhk2025b_msgs::msg::Swerve>::SharedPtr   swerve_subscriber_;
 };
 }  // namespace simulation
