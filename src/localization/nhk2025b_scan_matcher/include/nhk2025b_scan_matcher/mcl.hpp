6c7ac48ff9a5cc5b5586288432dea0152c855468
kazu-321 <kzs321kzs@gmail.com>
2025-05-09 22:20:33 +0900

    impl mcl

---
+++ src/localization/nhk2025b_scan_matcher/include/nhk2025b_scan_matcher/mcl.hpp
@@ -0,0 +1,89 @@
+#ifndef __mcl_hpp__
+#define __mcl_hpp__
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/utils.h>
+#include <tf2_ros/buffer.h>
+
+#include <random>
+#include <vector>
+#include <memory>
+#include <string>
+#include <algorithm>
+
+namespace mcl {
+
+struct Particle {
+  double x;
+  double y;
+  double theta;
+  double weight;
+};
+
+class mcl : public rclcpp::Node {
+public:
+  explicit mcl(const rclcpp::NodeOptions & options);
+
+private:
+  // コールバック
+  void map_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr map_msg);
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr scan_msg);
+  void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr pose_msg);
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr odom_msg);
+
+  // 内部処理
+  void initialize_particles_uniform();
+  void motion_update(const geometry_msgs::msg::PoseStamped & current,
+                     const geometry_msgs::msg::PoseStamped & last);
+  void sensor_update(const sensor_msgs::msg::LaserScan & scan);
+  void resample_particles();
+  geometry_msgs::msg::PoseStamped estimate_pose() const;
+
+  // ユーティリティ
+  bool is_pose_valid(double x, double y) const;
+  double compute_likelihood(const Particle & p,
+                            const sensor_msgs::msg::LaserScan & scan) const;
+  bool get_transform(const std::string & target_frame,
+                     const std::string & source_frame,
+                     const rclcpp::Time & time,
+                     geometry_msgs::msg::TransformStamped & transform_out) const;
+
+  // ノード内変数
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr particles_pub_;
+
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  std::vector<Particle> particles_;
+  nav_msgs::msg::OccupancyGrid::SharedPtr map_;
+  geometry_msgs::msg::PoseStamped current_pose_;
+  geometry_msgs::msg::PoseStamped last_pose_;
+
+  std::default_random_engine rng_;
+
+  // パラメータ
+  int num_particles_;
+  double motion_noise_lin_;
+  double motion_noise_ang_;
+  double resample_threshold_;
+};
+
+}  // namespace mcl
+
+#endif  // __mcl_hpp__
