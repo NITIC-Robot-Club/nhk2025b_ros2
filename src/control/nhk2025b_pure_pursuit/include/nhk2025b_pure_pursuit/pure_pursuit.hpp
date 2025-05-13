40d07967c082d839caf888168e5e43e5a2e4176f
kazu-321 <kzs321kzs@gmail.com>
2025-05-14 01:34:17 +0900

    カーブ減速追加

--- src/control/nhk2025b_pure_pursuit/include/nhk2025b_pure_pursuit/pure_pursuit.hpp
+++ src/control/nhk2025b_pure_pursuit/include/nhk2025b_pure_pursuit/pure_pursuit.hpp
@@ -8,32 +8,40 @@
 #include <nav_msgs/msg/path.hpp>
 
 namespace pure_pursuit {
+
 class pure_pursuit : public rclcpp::Node {
    public:
     pure_pursuit (const rclcpp::NodeOptions &options);
 
    private:
-    double lookahead_p_, angle_p_;
-    double max_speed_xy_m_s_;
-    double max_speed_z_rad_s_;
-    double max_acceleration_xy_m_s2_;
-    double lookahead_distance_;
+    // パラメータ
+    double lookahead_time_;            // 速度スケーリング用の時間 [s]
+    double min_lookahead_distance_;    // 最小lookahead距離 [m]
+    double max_lookahead_distance_;    // 最大lookahead距離 [m]
+    double angle_p_;                   // 角度ゲイン
+    double max_speed_xy_m_s_;          // 最大並進速度
+    double max_speed_z_rad_s_;         // 最大回転速度
+    double max_acceleration_xy_m_s2_;  // 最大加速度
+    double lookahead_distance_;        // 現在のlookahead距離 [m]（動的に計算される）
 
+    // 入力データ
     geometry_msgs::msg::PoseStamped  current_pose_;
     geometry_msgs::msg::TwistStamped last_cmd_vel_;
     nav_msgs::msg::Path              path_;
 
+    // コールバック
     void timer_callback ();
     void pose_callback (const geometry_msgs::msg::PoseStamped::SharedPtr msg);
     void path_callback (const nav_msgs::msg::Path::SharedPtr msg);
 
-    rclcpp::TimerBase::SharedPtr timer_;
-
+    // ROS2通信
+    rclcpp::TimerBase::SharedPtr                                     timer_;
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr   cmd_vel_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr    lookahead_publisher_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscriber_;
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr             path_subscriber_;
 };
+
 }  // namespace pure_pursuit
 
-#endif  //__pure_pursuit_hpp__
+#endif  // __pure_pursuit_hpp__
