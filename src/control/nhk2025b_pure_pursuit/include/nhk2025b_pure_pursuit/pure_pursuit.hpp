820cc6f5850e74993034d37415a0cc19a80990f3
kazu-321 <kzs321kzs@gmail.com>
2025-05-14 14:37:22 +0900

    角度用lookaheadを追加

--- src/control/nhk2025b_pure_pursuit/include/nhk2025b_pure_pursuit/pure_pursuit.hpp
+++ src/control/nhk2025b_pure_pursuit/include/nhk2025b_pure_pursuit/pure_pursuit.hpp
@@ -6,7 +6,6 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <nav_msgs/msg/path.hpp>
-#include <sensor_msgs/msg/laser_scan.hpp>
 
 namespace pure_pursuit {
 
@@ -19,6 +18,7 @@ class pure_pursuit : public rclcpp::Node {
     double lookahead_time_;            // 速度スケーリング用の時間 [s]
     double min_lookahead_distance_;    // 最小lookahead距離 [m]
     double max_lookahead_distance_;    // 最大lookahead距離 [m]
+    double angle_lookahead_distance_;    // 角度スケーリング用の距離 [m]
     double angle_p_;                   // 角度ゲイン
     double max_speed_xy_m_s_;          // 最大並進速度
     double max_speed_z_rad_s_;         // 最大回転速度
@@ -28,23 +28,20 @@ class pure_pursuit : public rclcpp::Node {
     // 入力データ
     geometry_msgs::msg::PoseStamped  current_pose_;
     geometry_msgs::msg::TwistStamped last_cmd_vel_;
-    geometry_msgs::msg::TwistStamped potential_cmd_vel_;
     nav_msgs::msg::Path              path_;
 
     // コールバック
     void timer_callback ();
     void pose_callback (const geometry_msgs::msg::PoseStamped::SharedPtr msg);
     void path_callback (const nav_msgs::msg::Path::SharedPtr msg);
-    void scan_callback (const sensor_msgs::msg::LaserScan::SharedPtr msg);
 
     // ROS2通信
     rclcpp::TimerBase::SharedPtr                                     timer_;
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr   cmd_vel_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr    lookahead_publisher_;
-    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr   potential_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr    lookahead2_publisher_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscriber_;
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr             path_subscriber_;
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr     scan_subscriber_;
 };
 
 }  // namespace pure_pursuit
--- src/control/nhk2025b_pure_pursuit/src/pure_pursuit.cpp
+++ src/control/nhk2025b_pure_pursuit/src/pure_pursuit.cpp
@@ -3,21 +3,20 @@
 namespace pure_pursuit {
 
 pure_pursuit::pure_pursuit (const rclcpp::NodeOptions &options) : Node ("pure_pursuit", options) {
-    cmd_vel_publisher_   = this->create_publisher<geometry_msgs::msg::TwistStamped> ("/cmd_vel", 10);
-    lookahead_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped> ("/control/lookahead_pose", 10);
-    potential_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped> ("/control/potential_cmd_vel", 10);
-    pose_subscriber_     = this->create_subscription<geometry_msgs::msg::PoseStamped> (
+    cmd_vel_publisher_    = this->create_publisher<geometry_msgs::msg::TwistStamped> ("/cmd_vel", 10);
+    lookahead_publisher_  = this->create_publisher<geometry_msgs::msg::PoseStamped> ("/control/lookahead_pose", 10);
+    lookahead2_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped> ("/control/lookahead_pose2", 10);
+    pose_subscriber_      = this->create_subscription<geometry_msgs::msg::PoseStamped> (
         "/localization/current_pose", 10, std::bind (&pure_pursuit::pose_callback, this, std::placeholders::_1));
     path_subscriber_ =
         this->create_subscription<nav_msgs::msg::Path> ("/planning/path", 10, std::bind (&pure_pursuit::path_callback, this, std::placeholders::_1));
-    scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan> (
-        "/sensor/scan", rclcpp::SensorDataQoS (), std::bind (&pure_pursuit::scan_callback, this, std::placeholders::_1));
 
     timer_ = this->create_wall_timer (std::chrono::milliseconds (50), std::bind (&pure_pursuit::timer_callback, this));
 
     this->declare_parameter ("lookahead_time", 1.0);
     this->declare_parameter ("min_lookahead_distance", 0.3);
     this->declare_parameter ("max_lookahead_distance", 3.0);
+    this->declare_parameter ("angle_lookahead_distance", 1.0);
     this->declare_parameter ("angle_p", 2.0);
     this->declare_parameter ("max_speed_xy_m_s", 3.0);
     this->declare_parameter ("max_speed_z_rad_s", 3.14);
@@ -32,37 +31,12 @@ void pure_pursuit::path_callback (const nav_msgs::msg::Path::SharedPtr msg) {
     path_ = *msg;
 }
 
-void pure_pursuit::scan_callback (const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    potential_cmd_vel_.twist.linear.x = 0.0;
-    potential_cmd_vel_.twist.linear.y = 0.0;
-    for (int i = 0; i < msg->ranges.size (); i++) {
-        double angle = msg->angle_min + i * msg->angle_increment;
-        double range = msg->ranges[i];
-        if (range < msg->range_min || range > msg->range_max) continue;
-
-        double x = range * std::cos (angle);
-        double y = range * std::sin (angle);
-
-        // 障害物からの力を計算
-        double robot_size = 0.5;
-        if (std::abs (x - last_cmd_vel_.twist.linear.x) > robot_size) continue;
-        if (std::abs (y - last_cmd_vel_.twist.linear.y) > robot_size) continue;
-        double distance = std::hypot (x, y);
-        potential_cmd_vel_.twist.linear.x -= x / (distance * distance);
-        potential_cmd_vel_.twist.linear.y -= y / (distance * distance);
-    }
-    potential_cmd_vel_.twist.linear.x /= 50;
-    potential_cmd_vel_.twist.linear.y /= 50;
-    potential_cmd_vel_.header.stamp    = this->now ();
-    potential_cmd_vel_.header.frame_id = "base_link";
-    potential_publisher_->publish (potential_cmd_vel_);
-}
-
 void pure_pursuit::timer_callback () {
     // パラメータ取得
     this->get_parameter ("lookahead_time", lookahead_time_);
     this->get_parameter ("min_lookahead_distance", min_lookahead_distance_);
     this->get_parameter ("max_lookahead_distance", max_lookahead_distance_);
+    this->get_parameter ("angle_lookahead_distance", angle_lookahead_distance_);
     this->get_parameter ("angle_p", angle_p_);
     this->get_parameter ("max_speed_xy_m_s", max_speed_xy_m_s_);
     this->get_parameter ("max_speed_z_rad_s", max_speed_z_rad_s_);
@@ -97,7 +71,9 @@ void pure_pursuit::timer_callback () {
     for (int i = closest_index; i < path_.poses.size (); i++) {
         double dist = std::hypot (
             path_.poses[i].pose.position.x - current_pose_.pose.position.x, path_.poses[i].pose.position.y - current_pose_.pose.position.y);
-        if (dist > lookahead_distance_) break;
+        if (dist > lookahead_distance_) {
+            break;
+        }
         lookahead_index = i;
     }
 
@@ -108,48 +84,86 @@ void pure_pursuit::timer_callback () {
 
     double current_yaw = std::asin (current_pose_.pose.orientation.z) * 2;
     double angle_diff  = std::atan2 (dy, dx) - current_yaw;
-    double yaw_diff    = std::asin (path_.poses[lookahead_index].pose.orientation.z) * 2 - current_yaw;
-    if (yaw_diff > M_PI) yaw_diff -= 2.0 * M_PI;
-    if (yaw_diff < -M_PI) yaw_diff += 2.0 * M_PI;
-
+    
     // 加速度制限付き速度推定
     double delta_t      = 0.05;  // 50ms
     double last_speed   = std::hypot (last_cmd_vel_.twist.linear.x, last_cmd_vel_.twist.linear.y);
     double target_speed = std::hypot (dx, dy) / delta_t;
+    
+    // ゴール付近での減速
+    constexpr double goal_tolerance_distance = 0.5;  // [m]
+    if (goal_distance < goal_tolerance_distance) {
+        target_speed *= goal_distance / goal_tolerance_distance;
+        if (goal_distance < 0.05) target_speed = 0.0;
+    }
+    
+    // 曲率に応じた速度制限
+    int p1 = closest_index;
+    int p2 = (lookahead_index + closest_index) / 2;
+    int p3 = lookahead_index;
+    
+    double a = std::hypot (
+        path_.poses[p1].pose.position.x - path_.poses[p2].pose.position.x, path_.poses[p1].pose.position.y - path_.poses[p2].pose.position.y);
+    double b = std::hypot (
+        path_.poses[p2].pose.position.x - path_.poses[p3].pose.position.x, path_.poses[p2].pose.position.y - path_.poses[p3].pose.position.y);
+    double c = std::hypot (
+        path_.poses[p1].pose.position.x - path_.poses[p3].pose.position.x, path_.poses[p1].pose.position.y - path_.poses[p3].pose.position.y);
+    double s         = (a + b + c) / 2.0;
+    double area      = std::sqrt (s * (s - a) * (s - b) * (s - c));
+    double curvature = 4.0 * area / (a * b * c);
+    if (area < 1e-6) curvature = 0.0;
+
+    double curvature_speed = target_speed / (std::abs (curvature * 10) + 1e-6);
+    // RCLCPP_INFO(this->get_logger (), "target speed: %f, curvature speed: %f", target_speed, curvature_speed);
+    target_speed = std::min (target_speed, curvature_speed);
+
+    // 最大速度制限
+    target_speed = std::clamp (target_speed, 0.0, max_speed_xy_m_s_);
+
+    // lookahead距離更新
+    lookahead_distance_ = std::clamp (lookahead_time_ * target_speed, min_lookahead_distance_, max_lookahead_distance_);
+
     double acceleration = (target_speed - last_speed) / delta_t;
     acceleration        = std::clamp (acceleration, -max_acceleration_xy_m_s2_, max_acceleration_xy_m_s2_);
     double speed        = last_speed + acceleration * delta_t;
 
-    // ゴール付近での減速
-    constexpr double goal_tolerance_distance = 0.5;  // [m]
-    if (goal_distance < goal_tolerance_distance) {
-        speed *= goal_distance / goal_tolerance_distance;
-        if (goal_distance < 0.05) speed = 0.0;
+    int angle_lookahead_index = closest_index;
+    for (int i = closest_index; i < path_.poses.size (); i++) {
+        double dist = std::hypot (
+            path_.poses[i].pose.position.x - current_pose_.pose.position.x, path_.poses[i].pose.position.y - current_pose_.pose.position.y);
+        if (dist > angle_lookahead_distance_) {
+            break;
+        }
+        angle_lookahead_index = i;
     }
-
-    // 最大速度制限
-    speed = std::clamp (speed, 0.0, max_speed_xy_m_s_);
-
-    // lookahead距離更新
-    lookahead_distance_ = std::clamp (lookahead_time_ * speed, min_lookahead_distance_, max_lookahead_distance_);
-
-    double yaw_speed = angle_p_ * yaw_diff;
+    if (angle_lookahead_index == closest_index) angle_lookahead_index = path_.poses.size () - 1;
+    double yaw_diff    = std::asin (path_.poses[angle_lookahead_index].pose.orientation.z) * 2 - current_yaw;
+    while (yaw_diff > M_PI) yaw_diff -= 2.0 * M_PI;
+    while (yaw_diff < -M_PI) yaw_diff += 2.0 * M_PI;
+    double yaw_speed    = angle_p_ * yaw_diff;
+    yaw_speed           = std::clamp (yaw_speed, -max_speed_z_rad_s_, max_speed_z_rad_s_);
 
     // Twist 発行
     geometry_msgs::msg::TwistStamped cmd_vel;
     cmd_vel.header.stamp    = this->now ();
     cmd_vel.header.frame_id = "base_link";
-    cmd_vel.twist.linear.x  = speed * std::cos (angle_diff) + potential_cmd_vel_.twist.linear.x;
-    cmd_vel.twist.linear.y  = speed * std::sin (angle_diff) + potential_cmd_vel_.twist.linear.y;
+    cmd_vel.twist.linear.x  = speed * std::cos (angle_diff);
+    cmd_vel.twist.linear.y  = speed * std::sin (angle_diff);
     cmd_vel.twist.angular.z = yaw_speed;
     cmd_vel_publisher_->publish (cmd_vel);
 
     // lookahead可視化
-    geometry_msgs::msg::PoseStamped lookahead_point;
-    lookahead_point.header.stamp    = this->now ();
-    lookahead_point.header.frame_id = "map";
-    lookahead_point.pose            = path_.poses[lookahead_index].pose;
-    lookahead_publisher_->publish (lookahead_point);
+    geometry_msgs::msg::PoseStamped lookahead_msg;
+    lookahead_msg.header.stamp    = this->now ();
+    lookahead_msg.header.frame_id = "map";
+    lookahead_msg.pose            = path_.poses[lookahead_index].pose;
+    lookahead_publisher_->publish (lookahead_msg);
+
+    geometry_msgs::msg::PoseStamped lookahead2_msg;
+    lookahead2_msg.header.stamp    = this->now ();
+    lookahead2_msg.header.frame_id = "map";
+    lookahead2_msg.pose            = path_.poses[p2].pose;
+    lookahead2_publisher_->publish (lookahead2_msg);
 
     last_cmd_vel_ = cmd_vel;
 }
