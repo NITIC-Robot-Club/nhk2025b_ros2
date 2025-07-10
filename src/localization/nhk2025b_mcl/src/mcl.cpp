629b0d426157b1ab7834a41e6289540cdf2ae05a
kazu-321 <kzs321kzs@gmail.com>
2025-07-10 13:37:30 +0900

    no return

--- src/localization/nhk2025b_mcl/src/mcl.cpp
+++ src/localization/nhk2025b_mcl/src/mcl.cpp
@@ -161,7 +161,7 @@ void mcl::timer_callback () {
         // estimated.pose.position.y    = last_estimated_pose_.pose.position.y + (current_pose_.position.y - last_pose_.position.y);
         // estimated.pose.orientation.z = last_estimated_pose_.pose.orientation.z + (current_pose_.orientation.z - last_pose_.orientation.z);
         // estimated.pose.orientation.w = last_estimated_pose_.pose.orientation.w + (current_pose_.orientation.w - last_pose_.orientation.w);
-        return;
+        // return;
     }
 
     pose_pub_->publish (estimated);
