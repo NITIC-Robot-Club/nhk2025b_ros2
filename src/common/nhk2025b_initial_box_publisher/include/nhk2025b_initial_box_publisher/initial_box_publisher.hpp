67dd2e274cc254836f69208cb1d9f304cda82e4a
HK777712 <hirokikawaguchi77@gmail.com>
2025-10-05 19:04:27 +0900

    add initial box publisher

---
+++ src/common/nhk2025b_initial_box_publisher/include/nhk2025b_initial_box_publisher/initial_box_publisher.hpp
@@ -0,0 +1,50 @@
+#ifndef __INITIAL_BOX_PUBLISHER_HPP__
+#define __INITIAL_BOX_PUBLISHER_HPP__
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "nhk2025b_msgs/msg/box_arm.hpp"
+#include "nhk2025b_msgs/msg/box_array.hpp"
+#include "nhk2025b_msgs/msg/command.hpp"
+#include "std_msgs/msg/bool.hpp"
+
+namespace initial_box_publisher {
+class initial_box_publisher : public rclcpp::Node {
+   public:
+    initial_box_publisher (const rclcpp::NodeOptions &options);
+
+   private:
+    bool                                                       is_red;
+    rclcpp::Publisher<nhk2025b_msgs::msg::BoxArray>::SharedPtr box_array_publisher_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr       is_red_subscriber_;
+    rclcpp::TimerBase::SharedPtr                               timer_;
+
+    void                         timer_callback ();
+    void                         is_red_callback (const std_msgs::msg::Bool::SharedPtr msg);
+    nhk2025b_msgs::msg::BoxArray get_initial_box_array ();
+
+    nhk2025b_msgs::msg::BoxArray             box_array;
+    std::vector<std::pair<std::string, int>> box_names = {
+        {"a", 0},
+        {"a", 1},
+        {"a", 2},
+        {"a", 3},
+        {"a", 4},
+        {"a", 5},
+        {"a", 6},
+        {"a", 7},
+        {"a", 8},
+        {"a", 9},
+        {"b", 0},
+        {"b", 1},
+        {"b", 2},
+        {"b", 3},
+        {"b", 4},
+        {"c", 0},
+        {"c", 1}
+    };
+};
+
+}  // namespace initial_box_publisher
+
+#endif  // __INITIAL_BOX_PUBLISHER_HPP__
---
+++ src/common/nhk2025b_initial_box_publisher/src/initial_box_publisher.cpp
@@ -0,0 +1,181 @@
+#include "nhk2025b_initial_box_publisher/initial_box_publisher.hpp"
+
+namespace initial_box_publisher {
+initial_box_publisher::initial_box_publisher (const rclcpp::NodeOptions &options) : Node ("initial_box_publisher", options) {
+    box_array = get_initial_box_array();
+    for (auto box_name : box_names) {
+        nhk2025b_msgs::msg::Box box;
+        std::string             param_name = box_name.first + std::to_string (box_name.second);
+        int                     id         = box_name.second;
+        box.info.id                        = id;
+        if (box_name.first == "a") {
+            box.info.type = nhk2025b_msgs::msg::BoxInfo::A;
+            box.size.x    = 0.3;
+            box.size.y    = 0.3;
+            box.size.z    = 0.3;
+        } else if (box_name.first == "b") {
+            box.info.type = nhk2025b_msgs::msg::BoxInfo::B;
+            box.size.x    = 0.4;
+            box.size.y    = 0.4;
+            box.size.z    = 0.4;
+        } else if (box_name.first == "c") {
+            box.info.type = nhk2025b_msgs::msg::BoxInfo::C;
+            box.size.x    = 0.5;
+            box.size.y    = 0.5;
+            box.size.z    = 0.5;
+        } else if (box_name.first == "d") {
+            box.info.type = nhk2025b_msgs::msg::BoxInfo::D;
+            box.size.x    = 0.2;
+            box.size.y    = 0.8;
+            box.size.z    = 0.2;
+        } else if (box_name.first == "e") {
+            box.info.type = nhk2025b_msgs::msg::BoxInfo::E;
+            box.size.x    = 0.6;
+            box.size.y    = 0.2;
+            box.size.z    = 0.6;
+        }
+        this->declare_parameter<double> (param_name + ".position.x", 0.0);
+        this->declare_parameter<double> (param_name + ".position.y", 0.0);
+        this->declare_parameter<double> (param_name + ".position.z", -1.0);
+        this->declare_parameter<double> (param_name + ".orientation.yaw", 0.0);
+        box.pose.position.x = this->get_parameter (param_name + ".position.x").as_double ();
+        box.pose.position.y = this->get_parameter (param_name + ".position.y").as_double ();
+        box.pose.position.z = this->get_parameter (param_name + ".position.z").as_double ();
+        double yaw          = this->get_parameter(param_name + ".orientation.yaw").as_double();
+        box.pose.orientation.z = std::sin (yaw / 2.0);
+        box.pose.orientation.w = std::cos (yaw / 2.0);
+        // Convert yaw to quaternion------------------------------------------------------------------------------------------------------------------
+
+        if (box.pose.position.z < 0.0) continue;
+        box_array.boxes.push_back (box);
+    }
+    box_array_publisher_ = this->create_publisher<nhk2025b_msgs::msg::BoxArray> ("/box_state", 1);
+    is_red_subscriber_   = this->create_subscription<std_msgs::msg::Bool> ("/is_red", 1, std::bind (&initial_box_publisher::is_red_callback, this, std::placeholders::_1));
+    timer_               = this->create_wall_timer (std::chrono::milliseconds (100), std::bind (&initial_box_publisher::timer_callback, this));
+}
+void initial_box_publisher::timer_callback () {
+    // Publish the message
+    box_array_publisher_->publish (box_array);
+}
+void initial_box_publisher::is_red_callback (const std_msgs::msg::Bool::SharedPtr msg) {
+    is_red = msg->data;
+}
+nhk2025b_msgs::msg::BoxArray initial_box_publisher::get_initial_box_array () {
+    nhk2025b_msgs::msg::BoxArray box_array_msg;
+    nhk2025b_msgs::msg::Box      box_msg;
+
+    // box_b
+    for (int i = 0; i < 3; ++i) {
+        nhk2025b_msgs::msg::Box box;
+        switch (i) {
+            case 0:
+                box.pose.position.x = 7.0;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 1:
+                box.pose.position.x = 7.4;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 2:
+                box.pose.position.x = 7.8;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+        }
+        box.size.x    = 0.4;
+        box.size.y    = 0.4;
+        box.size.z    = 0.4;
+        box.info.type = nhk2025b_msgs::msg::BoxInfo::B;
+        box.info.id   = 100 + i;
+        box_array_msg.boxes.push_back (box);
+    }
+    // box_c
+    for (int i = 0; i < 3; ++i) {
+        nhk2025b_msgs::msg::Box box;
+        switch (i) {
+            case 0:
+                box.pose.position.x = 9.75;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 1:
+                box.pose.position.x = 10.0;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.64;
+                break;
+            case 2:
+                box.pose.position.x = 10.25;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+        }
+        box.size.x    = 0.5;
+        box.size.y    = 0.5;
+        box.size.z    = 0.5;
+        box.info.type = nhk2025b_msgs::msg::BoxInfo::C;
+        box.info.id   = 100 + i;
+        box_array_msg.boxes.push_back (box);
+    }
+    // box_d
+    for (int i = 0; i < 3; ++i) {
+        nhk2025b_msgs::msg::Box box;
+        switch (i) {
+            case 0:
+                box.pose.position.x = 8.1;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 1:
+                box.pose.position.x = 8.3;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 2:
+                box.pose.position.x = 8.5;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+        }
+        box.size.x    = 0.2;
+        box.size.y    = 0.8;
+        box.size.z    = 0.2;
+        box.info.type = nhk2025b_msgs::msg::BoxInfo::D;
+        box.info.id   = 100 + i;
+        box_array_msg.boxes.push_back (box);
+    }
+    // box_e
+    for (int i = 0; i < 3; ++i) {
+        nhk2025b_msgs::msg::Box box;
+        switch (i) {
+            case 0:
+                box.pose.position.x = 8.75;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 1:
+                box.pose.position.x = 9.05;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+            case 2:
+                box.pose.position.x = 9.35;
+                box.pose.position.y = 5.25;
+                box.pose.position.z = 0.14;
+                break;
+        }
+        box.size.x    = 0.3;
+        box.size.y    = 1.0;
+        box.size.z    = 0.3;
+        box.info.type = nhk2025b_msgs::msg::BoxInfo::E;
+        box.info.id   = 100 + i;
+        box_array_msg.boxes.push_back (box);
+    }
+
+    return box_array_msg;
+}
+}  // namespace initial_box_publisher
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE (initial_box_publisher::initial_box_publisher)
